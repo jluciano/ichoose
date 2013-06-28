9e7ed44cbfdb9ce29843120f6a1018989697082d
Nicolau DePaula <ndepaula@albany.edu>
2013-06-28 16:26:32 +0000

    added conversion trigger

---
+++ data/source/ctg-albany-edu/exporter-database/version/2013-Jun-21/convert-exporter-database.sh
@@ -0,0 +1,169 @@
+#!/bin/bash
+#
+#3 <#> a <http://purl.org/twc/vocab/conversion/ConversionTrigger> ;
+#3     rdfs:seeAlso <https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger>,
+#3                  <https://github.com/timrdf/csv2rdf4lod-automation/blob/master/bin/cr-create-convert-sh.sh> .
+#
+# datasetID versionID (lastModDate):
+# exporter-database 2013-Jun-21 ()
+#--------------------------------------------------------------
+
+see="https://github.com/timrdf/csv2rdf4lod-automation/wiki/CSV2RDF4LOD-not-set"
+CSV2RDF4LOD_HOME=${CSV2RDF4LOD_HOME:?"not set; source my-csv2rdf4lod-source-me.sh or see \$see"}
+
+# The identifiers used to name the dataset that will be converted.
+#            (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-process-phase:-name)
+surrogate="http://ichoose.tw.rpi.edu" # Came from $CSV2RDF4LOD_BASE_URI when cr-create-conversion-trigger.sh created this script.
+sourceID="ctg-albany-edu"               # Came from directory name ../../../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)
+datasetID="exporter-database"             # Came from directory name ../../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions)
+datasetVersion="2013-Jun-21"        # DEPRECATED
+versionID="2013-Jun-21"             # Came from directory name ../ (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Directory-Conventions) renaming datasetVersion (deprecating datasetVersion)
+eID="1"                             # enhancement identifier
+if [[ "$1" == "-e" && $# -ge 2 ]]; then
+   eID="$2" # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Generating-enhancement-parameters
+   shift 2
+fi
+
+cr_justdoit="no"
+if [[ "$1" == "--force" ]]; then
+   cr_justdoit="yes" # see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger#--force
+   shift
+fi
+
+if [ -d doc/logs ]; then
+   dateInXSDDateTime.sh > doc/logs/conversion-trigger-last-pulled
+fi
+
+destDir="automatic"                 # convention has led to always be 'automatic'; the directory where the converted RDF is put.
+#--------------------------------------------------------------
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_CargoLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_CargoLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-cargolog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_CustomerLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_CustomerLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-customerlog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_ProductLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_ProductLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-productlog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_PurchaseLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_PurchaseLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-purchaselog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_SalesLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_SalesLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-saleslog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_ShipmentLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_ShipmentLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-shipmentlog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+# manual/Agromercado-FLOID6230.xls_SupplierLog.csv
+sourceDir="manual"
+datafile="Agromercado-FLOID6230.xls_SupplierLog.csv"
+data="$sourceDir/$datafile"
+# Bootstrap conversion parameters (see https://github.com/timrdf/csv2rdf4lod-automation/wiki/Conversion-trigger):
+subjectDiscriminator="agromercado-floid6230.xls-supplierlog"             # Additional part of URI for subjects created; must be URI-ready (e.g., no spaces).
+commentCharacter=""                 # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+cellDelimiter=","                   # ONLY one character; complain to http://sourceforge.net/projects/javacsv/ otherwise.
+header=                             # Line that header is on; only needed if not '1'. '0' means no header.
+dataStart=                          # Line that data starts; only needed if not immediately after header.
+repeatAboveIfEmptyCol=              # 'Fill in' value from row above for this column.
+onlyIfCol=                          # Do not process if value in this column is empty
+interpretAsNull=                    # NO SPACES
+dataEnd=                            # Line on which data stops; only needed if non-data bottom matter (legends, footnotes, etc).
+source $CSV2RDF4LOD_HOME/bin/convert.sh
+
+
+#-----------------------------------
+source $CSV2RDF4LOD_HOME/bin/convert-aggregate.sh
+# ^^ Note, this step can also be done manually using publish/bin/publish.sh
