ead506837d6f82adf6d5f68b97e4c54bde2783bb
Tim Lebo <timrdf@gmail.com>
2013-07-18 13:01:57 +0000

    cherry picked lodspeakr components

--- lodspeakr/settings.inc.php
+++ lodspeakr/settings.inc.php
@@ -11,6 +11,26 @@ $conf['ns']['local']   = 'http://ichoose.tw.rpi.edu';
 $conf['mirror_external_uris'] = false;
 
 // Cherry-picked components (see https://github.com/alangrafu/lodspeakr/wiki/Reuse-cherry-picked-components-from-other-repositories)
+$conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/void:Dataset';
+$conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/rdfs:Resource';
+$conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:LayerDataset';
+$conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:AbstractDataset';
+$conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:MetaDataset';
+// $conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/old.conv';
+$conf['components']['types'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/types/conv:VersionedDataset';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/graph';
+// $conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/tim';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/instances';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/sanity-checks';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/ckan-mirror-status';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/datasets';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/statistics';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/dataset-sample-graphs';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/search';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/vocabularies';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/classes';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/namedGraphs';
+$conf['components']['services'][] = '/home/ichoose/opt/prizms/lodspeakrs/twc-healthdata/lodspeakr/components/services/triples';
 
 // Variables in  can be used to store user info.
 // For examples, 'title' will be used in the header.
